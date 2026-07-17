1c478022291aba470c55e260bad0eeced6e480d4
Alberto <Alberto@DESKTOP-EAD3T0B>
2026-07-16 21:04:01 -0300

    Corregir metadatos dinamicos en React 19

--- src/componentes/Seo/Seo.jsx
+++ src/componentes/Seo/Seo.jsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet-async";
 function Seo({ titulo, descripcion }) {
     return (
         <Helmet>
-            <title>{titulo} | FunkoManía</title>
+            <title>{`${titulo} | FunkoManía`}</title>
             <meta name="description" content={descripcion} />
         </Helmet>
     );
